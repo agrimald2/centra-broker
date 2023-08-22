a54ddd3e6f8da9718cf5a9a81889ba447f985d7d
agrimal2 <alonso.grimaldo@vici.la>
2023-08-22 00:21:32 -0500

    Add Migration and Model CustomerType | DocumentType | User

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'document_number',
+        'document_type_id',
+        'phone_number',
     ];
 
     /**
@@ -42,4 +45,12 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /**
+     * Get the document type that belongs to the user.
+     */
+    public function documentType()
+    {
+        return $this->belongsTo(DocumentType::class);
+    }
 }
--- database/migrations/2023_08_21_212107_create_customer_types_table.php
+++ database/migrations/2023_08_21_212107_create_customer_types_table.php
@@ -8,20 +8,15 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
+     * 
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('customer_types', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->string('document_number');
-            $table->unsignedBigInteger('document_type_id');
-            $table->string('phone_number');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -30,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('customer_types');
     }
 };
