f27e5865b2c7106489717da97e30e7729dcc6df9
agrimal2 <agrimaldopci18@gmail.com>
2023-10-12 14:57:29 -0500

    build

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -22,5 +22,21 @@ class UsersTableSeeder extends Seeder
             'phone_number' => '934094501',
             'password' => Hash::make('password'),
         ]);
+        DB::table('users')->insert([
+            'name' => 'Jorge Leon',
+            'email' => 'jleon@centra.pe',
+            'document_type_id' => 1,
+            'document_number' => '00000001',
+            'phone_number' => '934094501',
+            'password' => Hash::make('Jleon2023'),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Franco',
+            'email' => 'franco@centra.pe',
+            'document_type_id' => 1,
+            'document_number' => '00000002',
+            'phone_number' => '934094501',
+            'password' => Hash::make('Franco123'),
+        ]);
     }
 }
